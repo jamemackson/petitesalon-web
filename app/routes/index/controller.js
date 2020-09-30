68bc6a2b464fa120e42242fdaf10d5ee7d410a21
jame mackson <jamemackson@icloud.com>
2020-09-30 02:05:31 -0500

    make home page refinements

--- app/routes/index/controller.js
+++ app/routes/index/controller.js
@@ -3,19 +3,19 @@ import Controller from '@ember/controller';
 export default class IndexController extends Controller {
   baseUrl = 'https://res.cloudinary.com/petite/image/upload/f_auto/fl_lossy/q_auto:eco/w_900';
   items = [
-    {
-      image: 'features/blvd',
-      title: 'new online booking system is LIVE',
-      content: `
-        <p>We have a brand new booking system powered by boulevard!</p>
-      `,
-      link: {
-        text: 'Give it a try and book an appointment now.',
-        url: '#book-now',
-        route: undefined,
-        model: undefined
-      }
-    },
+    // {
+    //   image: 'features/blvd',
+    //   title: 'new online booking system is LIVE',
+    //   content: `
+    //     <p>We have a brand new booking system powered by boulevard!</p>
+    //   `,
+    //   link: {
+    //     text: 'Give it a try and book an appointment now.',
+    //     url: '#book-now',
+    //     route: undefined,
+    //     model: undefined
+    //   }
+    // },
     {
       image: `features/contactless-pickup-available.png`,
       title: 'welcome to petite salon!',
@@ -34,8 +34,11 @@ export default class IndexController extends Controller {
       image: `features/petite-saint-paul-aged-front-door.png`,
       title: 'Visit us in St Paul',
       content: `
-        <p>Visit us in St Paul</p>
+        <h2>Visit us in St. Paul</h2>
         <p>Located in the heart of the mac/groveland neighborhood.</p>
+        <p>Petite Saint Paul is open Monday through Saturday.</p>
+        <p>Call <a href="tel:6516994959">(651) 699 - 4959</a> to book over the phone, or click below to book online.</p>
+        <p><a class="button" href="#book-now">BOOK ONLINE</a></p>
       `,
       link: {
         text: 'meet the artists',
@@ -48,11 +51,14 @@ export default class IndexController extends Controller {
       image: `features/petite-minneapolis-product-wall.jpg`,
       title: 'Visit us in Minneapolis',
       content: `
-        <p>Visit us in Minneapolis</p>
+        <h2>Visit us in Minneapolis</h2>
         <p>Located in the tangletown neighborhood.</p>
+        <p>Petite Minneapolis is open Monday through Saturday.</p>
+        <p>Call <a href="tel:6516994959">(651) 699 - 4959</a> to book over the phone, or click below to book online.</p>
+        <p><a class="button" href="#book-now">BOOK ONLINE</a></p>
       `,
       link: {
-        text: 'more info',
+        text: 'meet the artists',
         url: undefined, // trigger standard a link to this url
         route: 'minneapolis', // trigger a ember transition to this route
         model: undefined // optional model to pass to link to transition
